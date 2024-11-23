9c8638a0f0f56e50f0ed787b1707f63cbfeb9387
TotoGoat <22680150@cuautla.tecnm.mx>
2024-11-22 19:11:49 -0600

    Menu
    Texturas en el menu

--- Assets/Scripts/contenedor_botones.gd
+++ Assets/Scripts/contenedor_botones.gd
@@ -4,6 +4,7 @@ extends VBoxContainer
 @onready var button_2: Button = $botonIniciar
 @onready var button_3: Button = $botonCreditos
 @onready var button_4: Button = $botonSalir
+@onready var continuar: TextureButton = $Continuar
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -26,6 +27,8 @@ func _ready() -> void:
 	button_4.add_theme_color_override("font_hover_color",Color(0.568,0.756,0.352))
 	button_4.add_theme_color_override("font_pressed_color",Color(0.541,0.674,0.0588))
 	button_4.add_theme_color_override("font_focus_color",Color(0.541,0.674,0.0588))
+	add_theme_color_override("font_color_hover", get_theme_color("font_color", "Button"))
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
